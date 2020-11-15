3748cff088b4c20160a7db53c4dfbd4dc7f3dffe
samer tannoury <samer.tannoury@isae.edu.lb>
2020-11-15 13:36:39 +0200

    Create ExchangeValueRepository
    Initial Commit

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+
+  
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10001,'LBP','USD',0.00066,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10002,'EUR','USD',1.18,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10003,'AED','USD',0.27,0);
