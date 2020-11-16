daa7f6b24d7a1bb77283c2b7df2aa51263e3ed2b
samer tannoury <samer.tannoury@isae.edu.lb>
2020-11-16 19:57:56 +0200

    Create CurrencyExchangeProxy Interface

--- currency-calculation-service/src/main/java/com/myapp/currencycalculation/CurrencyCalculationServiceApplication.java
+++ currency-calculation-service/src/main/java/com/myapp/currencycalculation/CurrencyCalculationServiceApplication.java
@@ -2,8 +2,11 @@ package com.myapp.currencycalculation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@FeignClient("com.myapp.currencycalculation")
 public class CurrencyCalculationServiceApplication {
 
 	public static void main(String[] args) {
